618dcbb5cda62d9f6c8d425823bce1b01edf776d
Guilherme Valentin <guilherme.vsilva@al.infnet.edu.br>
2025-05-04 13:49:40 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+
+namespace LojaProdutos.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Produto> Produtos { get; set; }
+
+        public void OnGet()
+        {
+            Produtos = new List<Produto>
+            {
+                new Produto { Nome = "Notebook", Preco = 3500.00 },
+                new Produto { Nome = "Mouse Gamer", Preco = 250.00 },
+                new Produto { Nome = "Teclado Mecânico", Preco = 450.00 }
+            };
+        }
+
+        public class Produto
+        {
+            public string Nome { get; set; }
+            public double Preco { get; set; }
+        }
+    }
+}
---
+++ Pages/Sobre.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LojaProdutos.Pages
+{
+    public class SobreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
